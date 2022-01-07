33c4a4241202e2bdaeaf51f9dd8539ac9ecdb132
marp <Omespef1@hotmail.com>
2022-01-06 22:09:36 -0500

    Administración de tipos de mascotas

--- src/app/_metronic/configs/dynamic-aside-menu.config.ts
+++ src/app/_metronic/configs/dynamic-aside-menu.config.ts
@@ -4,25 +4,25 @@ export const DynamicAsideMenuConfig = {
       title: 'Dashboard',
       root: true,
       icon: 'flaticon2-architecture-and-city',
-      svg: './assets/media/svg/icons/Design/Layers.svg',
+      svg: './assets/media/svg/icons/Menu/Chart2.svg',
       page: '/dashboard',
       translate: 'MENU.DASHBOARD',
       bullet: 'dot',
     },
-    {
-      title: 'Layout Builder',
-      root: true,
-      icon: 'flaticon2-expand',
-      page: '/builder',
-      svg: './assets/media/svg/icons/Home/Library.svg'
-    },
+    // {
+    //   title: 'Layout Builder',
+    //   root: true,
+    //   icon: 'flaticon2-expand',
+    //   page: '/builder',
+    //   svg: './assets/media/svg/icons/Home/Library.svg'
+    // },
     { section: 'Configuration' },
     {
       title: 'User Management',
       root: true,
       bullet: 'dot',
       icon: 'flaticon2-user-outline-symbol',
-      svg: './assets/media/svg/icons/General/User.svg',
+      svg: './assets/media/svg/icons/Menu/User2.svg',
       page: '/user-management',
       submenu: [
         {
@@ -40,7 +40,7 @@ export const DynamicAsideMenuConfig = {
       root: true,
       bullet: 'dot',
       icon: 'flaticon2-correct',
-      svg: './assets/media/svg/icons/Home/Earth.svg',
+      svg: './assets/media/svg/icons/Menu/Pet.svg',
       page: '/pet-management',
       submenu: [
         {
@@ -53,74 +53,74 @@ export const DynamicAsideMenuConfig = {
         }
       ]
     },
-    {
-      title: 'User Profile',
-      root: true,
-      bullet: 'dot',
-      icon: 'flaticon2-user-outline-symbol',
-      svg: './assets/media/svg/icons/Communication/Add-user.svg',
-      page: '/user-profile',
-    },
-    { section: 'Custom' },
-    {
-      title: 'Wizards',
-      root: true,
-      bullet: 'dot',
-      icon: 'flaticon2-mail-1',
-      svg: './assets/media/svg/icons/Shopping/Box1.svg',
-      page: '/wizards',
-      submenu: [
-        {
-          title: 'Wizard 1',
-          page: '/wizards/wizard-1'
-        },
-        {
-          title: 'Wizard 2',
-          page: '/wizards/wizard-2'
-        },
-        {
-          title: 'Wizard 3',
-          page: '/wizards/wizard-3'
-        },
-        {
-          title: 'Wizard 4',
-          page: '/wizards/wizard-4'
-        },
-      ]
-    },
-    {
-      title: 'Error Pages',
-      root: true,
-      bullet: 'dot',
-      icon: 'flaticon2-list-2',
-      svg: './assets/media/svg/icons/Code/Warning-2.svg',
-      page: '/error',
-      submenu: [
-        {
-          title: 'Error 1',
-          page: '/error/error-1'
-        },
-        {
-          title: 'Error 2',
-          page: '/error/error-2'
-        },
-        {
-          title: 'Error 3',
-          page: '/error/error-3'
-        },
-        {
-          title: 'Error 4',
-          page: '/error/error-4'
-        },
-        {
-          title: 'Error 5',
-          page: '/error/error-5'
-        },
-        {
-          title: 'Error 6',
-          page: '/error/error-6'
-        },
-      ]
-    },
+    // {
+    //   title: 'User Profile',
+    //   root: true,
+    //   bullet: 'dot',
+    //   icon: 'flaticon2-user-outline-symbol',
+    //   svg: './assets/media/svg/icons/Communication/Add-user.svg',
+    //   page: '/user-profile',
+    // },
+    // { section: 'Custom' },
+    // {
+    //   title: 'Wizards',
+    //   root: true,
+    //   bullet: 'dot',
+    //   icon: 'flaticon2-mail-1',
+    //   svg: './assets/media/svg/icons/Shopping/Box1.svg',
+    //   page: '/wizards',
+    //   submenu: [
+    //     {
+    //       title: 'Wizard 1',
+    //       page: '/wizards/wizard-1'
+    //     },
+    //     {
+    //       title: 'Wizard 2',
+    //       page: '/wizards/wizard-2'
+    //     },
+    //     {
+    //       title: 'Wizard 3',
+    //       page: '/wizards/wizard-3'
+    //     },
+    //     {
+    //       title: 'Wizard 4',
+    //       page: '/wizards/wizard-4'
+    //     },
+    //   ]
+    // },
+    // {
+    //   title: 'Error Pages',
+    //   root: true,
+    //   bullet: 'dot',
+    //   icon: 'flaticon2-list-2',
+    //   svg: './assets/media/svg/icons/Code/Warning-2.svg',
+    //   page: '/error',
+    //   submenu: [
+    //     {
+    //       title: 'Error 1',
+    //       page: '/error/error-1'
+    //     },
+    //     {
+    //       title: 'Error 2',
+    //       page: '/error/error-2'
+    //     },
+    //     {
+    //       title: 'Error 3',
+    //       page: '/error/error-3'
+    //     },
+    //     {
+    //       title: 'Error 4',
+    //       page: '/error/error-4'
+    //     },
+    //     {
+    //       title: 'Error 5',
+    //       page: '/error/error-5'
+    //     },
+    //     {
+    //       title: 'Error 6',
+    //       page: '/error/error-6'
+    //     },
+    //   ]
+    // },
   ]
 };
