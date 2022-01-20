fb6f519ae768a7744ee076b1f7cc4dd8fad93ed2
droldan <droldan@indigo.tech>
2022-01-20 11:59:15 -0500

    agregando tema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import {
-  Component,
-  ChangeDetectionStrategy,
-  OnDestroy,
-  OnInit,
+	Component,
+	ChangeDetectionStrategy,
+	OnDestroy,
+	OnInit,
 } from '@angular/core';
 import { TranslationService } from './modules/i18n/translation.service';
 // language list
@@ -17,53 +17,53 @@ import { Router, NavigationEnd, NavigationError } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { TableExtendedService } from './_metronic/shared/crud-table';
 @Component({
-  // tslint:disable-next-line:component-selector
-  selector: 'body[root]',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+	// tslint:disable-next-line:component-selector
+	selector: 'body[root]',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit, OnDestroy {
-  private unsubscribe: Subscription[] = []; // Read more: => https://brianflove.com/2016/12/11/anguar-2-unsubscribe-observables/
+	private unsubscribe: Subscription[] = []; // Read more: => https://brianflove.com/2016/12/11/anguar-2-unsubscribe-observables/
 
-  constructor(
-    private translationService: TranslationService,
-    private splashScreenService: SplashScreenService,
-    private router: Router,
-    private tableService: TableExtendedService
-  ) {
-    // register translations
-    this.translationService.loadTranslations(
-      enLang,
-      chLang,
-      esLang,
-      jpLang,
-      deLang,
-      frLang
-    );
-  }
+	constructor(
+		private translationService: TranslationService,
+		private splashScreenService: SplashScreenService,
+		private router: Router,
+		private tableService: TableExtendedService
+	) {
+		// register translations
+		this.translationService.loadTranslations(
+			enLang,
+			chLang,
+			esLang,
+			jpLang,
+			deLang,
+			frLang
+		);
+	}
 
-  ngOnInit() {
-    const routerSubscription = this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        // clear filtration paginations and others
-        this.tableService.setDefaults();
-        // hide splash screen
-        this.splashScreenService.hide();
+	ngOnInit() {
+		const routerSubscription = this.router.events.subscribe((event) => {
+			if (event instanceof NavigationEnd) {
+				// clear filtration paginations and others
+				this.tableService.setDefaults();
+				// hide splash screen
+				this.splashScreenService.hide();
 
-        // scroll to top on every route change
-        window.scrollTo(0, 0);
+				// scroll to top on every route change
+				window.scrollTo(0, 0);
 
-        // to display back the body content
-        setTimeout(() => {
-          document.body.classList.add('page-loaded');
-        }, 500);
-      }
-    });
-    this.unsubscribe.push(routerSubscription);
-  }
+				// to display back the body content
+				setTimeout(() => {
+					document.body.classList.add('page-loaded');
+				}, 500);
+			}
+		});
+		this.unsubscribe.push(routerSubscription);
+	}
 
-  ngOnDestroy() {
-    this.unsubscribe.forEach((sb) => sb.unsubscribe());
-  }
+	ngOnDestroy() {
+		this.unsubscribe.forEach((sb) => sb.unsubscribe());
+	}
 }
