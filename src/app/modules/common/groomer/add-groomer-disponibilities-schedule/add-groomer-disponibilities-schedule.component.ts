24400f989f0a6976357d2c2d7828c6c3b58247d9
darl8910 <darl_8910@hotmail.com>
2022-01-21 20:30:31 -0500

    implementacion del componente horario para disponibilidades

--- src/app/modules/common/groomer/add-groomer-disponibilities-schedule/add-groomer-disponibilities-schedule.component.ts
+++ src/app/modules/common/groomer/add-groomer-disponibilities-schedule/add-groomer-disponibilities-schedule.component.ts
@@ -1,9 +1,8 @@
-import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { GroomerDisponibilityModel } from 'src/app/_metronic/core/models/groomer-disponibility.model';
 import { SwalService } from 'src/app/_metronic/core/services/swal.service';
 import { GroomerService } from '../../services/groomer.service';
-import { DxSchedulerComponent } from 'devextreme-angular';
 import { RefreshGroomerDisponibilitiesService } from '../add-disponibilities/refresh-groomer-disponibilities.service';
 import { TranslateService } from '@ngx-translate/core';
 import { tap } from 'rxjs/operators';
@@ -20,51 +19,11 @@ export class AddGroomerDisponibilitiesScheduleComponent
 	@Input() groomerId: number;
 	data: AppointmentData[] = [];
 	isLoading$: Observable<boolean>;
-	currentDate: Date = new Date(1901, 0, 1);
-	@ViewChild(DxSchedulerComponent, { static: false })
-	scheduler: DxSchedulerComponent;
-	days = [
-		{
-			id: 1,
-			text: this.translateService.instant('COMMON.MONDAY').toUpperCase(),
-		},
-		{
-			id: 2,
-			text: this.translateService.instant('COMMON.TUESDAY').toUpperCase(),
-		},
-		{
-			id: 3,
-			text: this.translateService
-				.instant('COMMON.WEDNESDAY')
-				.toUpperCase(),
-		},
-		{
-			id: 4,
-			text: this.translateService
-				.instant('COMMON.THURSDAY')
-				.toUpperCase(),
-		},
-		{
-			id: 5,
-			text: this.translateService.instant('COMMON.FRIDAY').toUpperCase(),
-		},
-		{
-			id: 6,
-			text: this.translateService
-				.instant('COMMON.SATURDAY')
-				.toUpperCase(),
-		},
-		{
-			id: 7,
-			text: this.translateService.instant('COMMON.SUNDAY').toUpperCase(),
-		},
-	];
 	subscriptions: Subscription[] = [];
 
 	constructor(
 		private readonly refreshGroomerDisponibilitiesService: RefreshGroomerDisponibilitiesService,
 		private readonly groomerService: GroomerService,
-		private readonly translateService: TranslateService,
 		private readonly swal: SwalService
 	) {}
 
@@ -87,22 +46,19 @@ export class AddGroomerDisponibilitiesScheduleComponent
 	}
 
 	onAddAppointment(e) {
-		console.log(e);
-		this.addDisponibility(e.appointmentData);
+		this.addDisponibility(e);
 	}
 
 	async onEditAppointment(e) {
 		try {
-			await this.editDisponibility(e.newData);
+			await this.editDisponibility(e);
 		} catch (e) {
-			e.cancel = true;
 			this.searchAllDisponibilities();
 		}
 	}
 
 	onRemoveAppointment(e) {
-		e.cancel = true;
-		this.deleteDisponibility(e.appointmentData);
+		this.deleteDisponibility(e);
 	}
 
 	processData(disponibilities: GroomerDisponibilityModel[]) {
@@ -184,80 +140,5 @@ export class AddGroomerDisponibilitiesScheduleComponent
 				})
 			)
 			.toPromise();
-	}
-
-	convertToMinutes(startDate: Date, endDate: Date) {
-		const resta = endDate.getTime() - startDate.getTime();
-		return Math.round(resta / (1000 * 60));
-	}
-
-	onAppointmentFormOpening(data) {
-		const that = this;
-		const form = data.form;
-		// let movieInfo = that.getMovieById(data.appointmentData.movieId) || {};
-		const duration = 30;
-		let startDate = data.appointmentData.startDate;
-
-		form.option('items', [
-			{
-				label: {
-					text: this.translateService.instant('GROOMER.START_DATE'),
-				},
-				dataField: 'startDate',
-				editorType: 'dxDateBox',
-				editorOptions: {
-					// width: '100%',
-					// type: 'datetime',
-					// onValueChanged(args) {
-					// 	startDate = args.value;
-					// 	form.updateData(
-					// 		'endDate',
-					// 		new Date(startDate.getTime() + 60 * 1000 * duration)
-					// 	);
-					// },
-					width: '100%',
-					type: 'time',
-					pickerType: 'calendar',
-					readOnly: false,
-				},
-			},
-			{
-				label: {
-					text: this.translateService.instant('GROOMER.END_DATE'),
-				},
-				name: 'endDate',
-				dataField: 'endDate',
-				editorType: 'dxDateBox',
-				editorOptions: {
-					width: '100%',
-					type: 'time',
-					pickerType: 'calendar',
-					readOnly: false,
-				},
-			},
-			{
-				label: {
-					text: this.translateService.instant('GROOMER.DAY'),
-				},
-				editorType: 'dxSelectBox',
-				dataField: 'dayOfWeek',
-				editorOptions: {
-					items: that.days,
-					displayExpr: 'text',
-					valueExpr: 'id',
-					onValueChanged(args) {
-						// movieInfo = that.getMovieById(args.value);
-						// form.updateData('director', movieInfo.director);
-						// form.updateData(
-						// 	'endDate',
-						// 	new Date(
-						// 		startDate.getTime() +
-						// 			60 * 1000 * movieInfo.duration
-						// 	)
-						// );
-					},
-				},
-			},
-		]);
 	}
 }
